04372178d38f2b4ce767891466d753d12f174b5f
JolouTheRookie <barrientos.134038@iloilo.sti.edu.ph>
2020-08-05 11:03:41 +0800

    Added a cards

---
+++ js/card.js
@@ -0,0 +1,29 @@
+/*
+*   Here are the collections of the cards located
+*   Has array of card which have properties of a name and image
+*   The images are chibi's because i like anime characters
+*   that are cute 
+*/
+
+let cards = [
+    {
+        name: 'c1',
+        img: 'img/cards/c1.png'
+    },
+    {
+        name: 'c2',
+        img: 'img/cards/c2.png'
+    },
+    {
+        name: 'c3',
+        img: 'img/cards/c3.png'
+    },
+    {
+        name: 'c4',
+        img: 'img/cards/c4.png'
+    },
+    {
+        name: 'c7',
+        img: 'img/cards/c7.png'
+    }
+]
